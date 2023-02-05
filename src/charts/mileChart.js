05684566aaaa52c7e5437c9234f4247dc153d9cf
Atharva Bhawsar <atharvabhawsar18@gmail.com>
2023-02-05 11:27:20 +0530

    bar chart dummy import

--- src/charts/mileChart.js
+++ src/charts/mileChart.js
@@ -1,6 +1,23 @@
+import React from "react";
+import { Bar, BarChart, ResponsiveContainer, Tooltip, XAxis } from "recharts";
+// import { ResponsiveContainer, BarChart, Bar, XAxis, Tooltip } from "recahrts";
+import mileStaticsData from "../assets/dummy-data/mileStatics"
+
 function mileChart() {
-  return (
-    <div></div>
-  )
+    return (
+        <ResponsiveContainer width='100%'>
+            <BarChart data={mileStaticsData}>
+                <XAxis datakey="mileStats" stroke="#2884ff" />
+                <Bar
+                    dataKey="mileStats"
+                    stroke="#2884ff"
+                    fill="#2884ff"
+                    barSize={30} />
+                <Tooltip
+                    wrapperClassName="tooltip__style"
+                    cursor={false} />
+            </BarChart>
+        </ResponsiveContainer>
+    );
 }
 export default mileChart
